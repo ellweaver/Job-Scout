82b3a2fbd2578cf5f3c7e099b4a720488e5b00d9
ellweaver <elliotweaver247@gmail.com>
2025-11-18 15:00:40 +0000

    created transform.py

---
+++ src/transform.py
@@ -0,0 +1,10 @@
+import json
+
+
+def create_headlines(event):
+     for job in event["data"]:
+          job["job_title"]
+          job["job_location"]
+          job["job_salary"]  
+          job["employer_name"]
+          job["job_highlights"]["Responsibilities"]  
